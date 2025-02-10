e44a4f1e7465df7d9df620532d82b91e1fdb0d5b
arw6329 <70402863+arw6329@users.noreply.github.com>
2025-02-10 15:09:28 -0800

    Add working gender markers (F/M), relationship to root in profile header

--- src/components/family-tree/profile-header/ProfileHeader.tsx
+++ src/components/family-tree/profile-header/ProfileHeader.tsx
@@ -1,25 +1,36 @@
 import HeaderButton from "@/components/header-button/HeaderButton"
 import "./ProfileHeader.scoped.css"
 import { Profile } from "@/lib/family-tree/FamilyTreeDatabase"
-import { FaVenus } from "react-icons/fa"
+import { FaMars, FaVenus } from "react-icons/fa"
 import { useContext } from "react"
 import { FamilyTreeStateContext } from "../FamilyTreeState"
 import { ProfileNode } from "@/lib/family-tree/ProfileNode"
+import { IconContext } from "react-icons"
+import { relation_to } from "@/lib/family-tree/relation"
 
 const ProfileHeader: React.FC<{ node: ProfileNode }> = ({ node }) => {
     const state = useContext(FamilyTreeStateContext)
     const profile: Profile = node.data.profile
+
+    const relationToRoot = relation_to(node, state.rootNode)
 
     return (
         <header>
             <div className="row">
                 <img className="profile-pic" src="https://reunionpage.net/sprites/reunionpage-logo.png" alt={`${profile.name}`} />
                 <div style={{ display: 'flex', flexDirection: 'column', gap: '6px' }}>
-                    <div style={{ display: 'flex', gap: '6px' }}>
+                    <div style={{ display: 'flex', gap: '6px', alignItems: 'center' }}>
                         <span className="name">{profile.name}</span>
-                        <FaVenus fill="#ffb3c0" height={50} width={100} />
+                        {profile.family_tree_gender === 'FEMALE'
+                            && <IconContext.Provider value={{ style: { height: 25 } }}>
+                                <FaVenus fill="#ffb3c0" />
+                            </IconContext.Provider>}
+                        {profile.family_tree_gender === 'MALE'
+                            && <IconContext.Provider value={{ style: { height: 27, width: 22 } }}>
+                                <FaMars fill="cornflowerblue" />
+                            </IconContext.Provider>}
                     </div>
-                    <span className="relationship">{profile.name}'s sister</span>
+                    {relationToRoot && <span className="relationship">{state.rootNode.data.profile.name}'s {relationToRoot.text}</span>}
                 </div>
             </div>
             <div className="row">
